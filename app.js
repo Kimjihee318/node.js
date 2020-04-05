ef160a30c313f4bcaeb126ef730c0d8f50f3b865
jihee kim <jiheekim318@gmail.com>
2020-04-05 23:54:15 +0900

    Ajax post

--- app.js
+++ app.js
@@ -28,7 +28,15 @@ app.get('/main', (req, res) => {
   res.sendFile(__dirname + "/public/main.html")
 })
 
+// * form.html form action 에서 사용하는 url 구성
 app.post('/email_post', (req, res) => {
   console.log(req.body);
   res.render('email.ejs', {'email' : req.body.email})
+})
+
+// * form.html post에서 사용하는 url 구성
+app.post('/ajax_send_email', (req, res) => {
+  console.log(req.body);
+  let responseData = { 'result' : 'ok', 'email' : req.body.email }
+  res.json(responseData)
 })
