be2d244d6128c7d58f4542dd3b8e978b41fca833
jihee kim <jiheekim318@gmail.com>
2020-04-12 23:47:25 +0900

    Implement Passport

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let express = require('express')
 let app = express()
 let router = require('./router')
+let passport = require('passport')
+let localStrategy = require('passport-local').Strategy
+let session = require('express-session')
+let flash = require('connect-flash')
 let service = require('./app.module/service/middleware')
 
 // Terminal use "nodemon app.js"
@@ -12,5 +16,18 @@ app.listen(3000, () => {
 // * Set middleware
 app = new service(app, express)
 
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+// init
+app.use(passport.initialize())
+app.use(flash())
+
 // * Set router
 app.use(router)
+
+// app.use를 이용해 middleware사용
--- router/join/index.js
+++ router/join/index.js
@@ -1,4 +1,7 @@
 let app = require('../config')
+let passport = require('passport')
+let LocalStrategy = require('passport-local').Strategy
+
 let Mysql = require('../../app.module/database/mysql')
 
 let mysqlInstance = new Mysql()
@@ -10,30 +13,40 @@ app.router.get('/', (req, res) => {
   res.render('join.ejs')
 })
 
-app.router.post('/', (req, res) => {
-  let body = req.body
-  // TODO destructure
-  // let id = body.id
-  let email = body.email
-  let name = body.name
-  let passwd = body.passwd
 
-  // * s
-  let sql = {no: null, email: email, name: name, pw: passwd}
+// * http://www.passportjs.org/
+passport.use('local-join', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'passwd',
+  passReqToCallback: true
+}, function(req, email, password, done) {
+  console.log(`local-join callback called`);
+}))
 
-  // no.1 | NOT USING ESCAPE | let query = mysqlInstance.getConnection().query(`INSERT INTO user (id,email,name,pw) VALUES ("${id}","${email}","${name}","${passwd}")`, (err, rows) => {
-  // 변수마다 쌍 따옴표로 감싸야 MYSQL에서 문자로 인식한다.
-  // https://m.blog.naver.com/PostView.nhn?blogId=eyeballss&logNo=220707298708&proxyReferer=https:%2F%2Fwww.google.com%2F
-  // * MYSQL ESCAPE를 사용하여 쿼리 정리 | injection 공격에 효과적임
-  // * https://github.com/mysqljs/mysql | escaping queries documentation
-  let query = mysqlInstance.getConnection().query(`INSERT INTO user set ?`, sql , (err, rows) => {
-    if(err) {
-      console.log(err);
-      throw err
-      // TODO
-      return
-    }
-    res.render('join_res.ejs', {'no' : rows.insertId, 'name': name})
-  })
-})
+// app.router.post('/', (req, res) => {
+//   let body = req.body
+//   // TODO destructure
+//   // let id = body.id
+//   let email = body.email
+//   let name = body.name
+//   let passwd = body.passwd
+
+//   // * s
+//   let sql = {no: null, email: email, name: name, pw: passwd}
+
+//   // no.1 | NOT USING ESCAPE | let query = mysqlInstance.getConnection().query(`INSERT INTO user (id,email,name,pw) VALUES ("${id}","${email}","${name}","${passwd}")`, (err, rows) => {
+//   // 변수마다 쌍 따옴표로 감싸야 MYSQL에서 문자로 인식한다.
+//   // https://m.blog.naver.com/PostView.nhn?blogId=eyeballss&logNo=220707298708&proxyReferer=https:%2F%2Fwww.google.com%2F
+//   // * MYSQL ESCAPE를 사용하여 쿼리 정리 | injection 공격에 효과적임
+//   // * https://github.com/mysqljs/mysql | escaping queries documentation
+//   let query = mysqlInstance.getConnection().query(`INSERT INTO user set ?`, sql , (err, rows) => {
+//     if(err) {
+//       console.log(err);
+//       throw err
+//       // TODO
+//       return
+//     }
+//     res.render('join_res.ejs', {'no' : rows.insertId, 'name': name})
+//   })
+// })
 module.exports = app.router
